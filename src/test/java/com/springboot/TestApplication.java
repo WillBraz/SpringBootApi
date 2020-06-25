d9715e9011629b5e5c30e780a6de19a0330cddce
willian_braz <willianlpbraz@gmail.com>
2020-06-25 11:46:27 -0300

    configuracão inicial para os testes

---
+++ src/test/java/com/springboot/TestApplication.java
@@ -0,0 +1,16 @@
+package com.springboot;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TestApplication {
+    
+    @Test
+    public void testHelloWorld(){
+        assertEquals(1,1);
+    }
+}
